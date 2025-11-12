7e65220c1076b2b3e332c8ed790c5c8cc6a03975
José Pablo GR <granadosrovira1824@ciencias.unam.mx>
2025-11-12 08:08:22 -0600

    Crear playlists

--- hitmeup/playlists/urls.py
+++ hitmeup/playlists/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('list-playlists', views.playlists_list, name= 'list-playlists'),
     path('<int:playlist_id>', views.playlist_detail, name= 'playlist'),
     path('search-lists', views.playlists_search, name= 'search-lists'),
+    path('artist/<int:artist_id>/love', views.artist_love_playlist, name= 'playlist_artist_love'),
+    path('artist/<int:artist_id>/love/sync', views.artist_love_sync, name= 'playlist_artist_love_sync'),
+    path('new', views.create_playlist_user, name='playlist_create_user'),
+    path('new/artist', views.create_playlist_artist, name='playlist_create_artist'),
 ]
 
