4b8e68d471f603cc3ff86d1f35e5de9c4101b562
José Pablo GR <granadosrovira1824@ciencias.unam.mx>
2025-11-13 07:40:02 -0600

    Ver canciones

--- hitmeup/songs/urls.py
+++ hitmeup/songs/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('delete/<int:song_id>', views.delete_song, name= 'songs_delete'),
     path('like/<int:song_id>', views.toggle_like_user, name= 'songs_like_user'),
     path('liked', views.liked_songs, name= 'songs_liked'),
+    path('explore', views.explore_songs, name='songs_explore'),
+    path('<int:song_id>', views.song_detail, name='song_detail'),
 ]
